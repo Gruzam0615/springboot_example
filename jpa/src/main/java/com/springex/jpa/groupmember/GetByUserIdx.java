1ee78ec9716da08ea44bc3bcfe24cfcbf04abcb5
gruzam0615 <gruzam0615@gmail.com>
2022-08-15 14:20:29 +0900

    불필요한 파일 정리

--- jpa/src/main/java/com/springex/jpa/groupmember/GetByUserIdx.java
+++ jpa/src/main/java/com/springex/jpa/groupmember/GetByUserIdx.java
@@ -1,8 +1,13 @@
 package com.springex.jpa.groupmember;
 
+import java.time.LocalDateTime;
+
 public interface GetByUserIdx {
 
     Long getGroupMemberIdx();
+    Long getGroupIdx();
+    Long getUserIdx();
     String getUserName();
+    LocalDateTime getJoinDate();
 
 }
