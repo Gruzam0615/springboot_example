4ddda55182f5b887408c1cc0223683f27d765b55
gruzam0615 <gruzam0615@gmail.com>
2023-11-03 02:45:25 +0900

    delete prev example "securitybasic"

--- 2.7.x/security-basic/src/main/resources/static/sql/UserEntityData.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO UserEntity (userAccount, userPass, userName, userMobile, userEmail, userRole, userJoinDate) VALUES ('testuser01', 'testuser01', 'testuser01', '01010030100', 'testuser01@email.com', 'CLIENT', NOW())
