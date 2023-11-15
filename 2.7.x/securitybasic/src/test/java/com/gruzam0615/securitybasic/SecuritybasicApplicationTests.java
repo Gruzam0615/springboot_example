60201a085669f3c062c7572ede188785db8b8835
gruzam0615 <gruzam0615@gmail.com>
2023-11-15 21:32:30 +0900

    2.7.x > security_basic init

---
+++ 2.7.x/securitybasic/src/test/java/com/gruzam0615/securitybasic/SecuritybasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gruzam0615.securitybasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritybasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
