242c481a3e49cf23f402935267a7fa889fa87ab2
gruzam0615 <gruzam0615@gmail.com>
2023-11-22 04:44:17 +0900

    spring security REST_API 예제 프로젝트 생성

---
+++ 2.7.x/securitybasic2/src/main/java/com/gruzam0615/securitybasic2/Securitybasic2Application.java
@@ -0,0 +1,13 @@
+package com.gruzam0615.securitybasic2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Securitybasic2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Securitybasic2Application.class, args);
+	}
+
+}
---
+++ 2.7.x/securitybasic2/src/test/java/com/gruzam0615/securitybasic2/Securitybasic2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gruzam0615.securitybasic2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Securitybasic2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
