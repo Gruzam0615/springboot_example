27f12a3ee490d07d416a27827bb2ade586d26a1e
gruzam0615 <gruzam0615@gmail.com>
2023-12-02 01:37:11 +0900

    securityoauth2 project created

---
+++ 3.x.x/securityoauth2/src/main/java/com/gruzam0615/securityoauth2/util/CustomResponseEntity.java
@@ -0,0 +1,27 @@
+package com.gruzam0615.securityoauth2.util;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class CustomResponseEntity {
+
+    String message;
+    Object data;
+    LocalDateTime timeStamp;
+
+    public CustomResponseEntity(String message, LocalDateTime timeStamp) {
+        this.message = message;
+        this.timeStamp = timeStamp;
+    }
+
+    public CustomResponseEntity(String message, Object data, LocalDateTime timeStamp) {
+        this.message = message;
+        this.data = data;
+        this.timeStamp = timeStamp;
+    }
+    
+}
