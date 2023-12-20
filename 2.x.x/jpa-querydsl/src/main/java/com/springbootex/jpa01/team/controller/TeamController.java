fdaf6521e5b388b5751c531d1d0b9e756d94c7d3
gruzam0615 <gruzam0615@gmail.com>
2023-12-20 22:56:46 +0900

    ...

--- 2.x.x/jpa-querydsl/src/main/java/com/springbootex/jpa01/team/controller/TeamController.java
+++ 2.x.x/jpa-querydsl/src/main/java/com/springbootex/jpa01/team/controller/TeamController.java
@@ -1,12 +1,14 @@
 package com.springbootex.jpa01.team.controller;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.springbootex.jpa01.member.entity.Member;
 import com.springbootex.jpa01.member.entity.MemberRegistParam;
-import com.springbootex.jpa01.member.entity.QMember;
 import com.springbootex.jpa01.team.entity.QTeam;
 import com.springbootex.jpa01.team.entity.Team;
+import com.springbootex.jpa01.team.service.TeamService;
+
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +26,9 @@ public class TeamController {
     @PersistenceContext
     EntityManager em;
 
+    @Autowired
+    private TeamService teamService;
+
     @GetMapping(value = "/list")
     public String teamList() {
         return "team/list";
@@ -38,14 +43,16 @@ public class TeamController {
     @ResponseBody
     public String teamRegistAction(MemberRegistParam param) {
         log.info("param: " + param.toString());
+        Team input = new Team(param.getTeamName());
+        Team t = teamService.save(input);
 
-        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
-        QTeam t = new QTeam("t");
+        // JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        // QTeam t = new QTeam("t");
 
-        queryFactory.insert(t)
-                .columns(t.teamName)
-                .values("team01")
-                .execute();
+        // queryFactory.insert(t)
+        //         .columns(t.teamName)
+        //         .values("team01")
+        //         .execute();
 
         return "teamRegistAction";
     }
--- 2.x.x/jpa-querydsl/src/main/java/com/springbootex/jpa01/team/service/TeamService.java
+++ 2.x.x/jpa-querydsl/src/main/java/com/springbootex/jpa01/team/service/TeamService.java
@@ -15,7 +15,10 @@ import org.springframework.stereotype.Service;
 import com.springbootex.jpa01.team.entity.Team;
 import com.springbootex.jpa01.team.repository.TeamRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class TeamService implements TeamRepository {
 
     @Autowired
